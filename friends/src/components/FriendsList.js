7f2dbc3f230d0b757f35198439345e93b1ffcd1b
Brian Illes <brianrilles@gmail.com>
2019-02-27 13:18:17 -0800

    styling

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -3,12 +3,15 @@ import React from "react";
 export default function FriendsList(props) {
   return (
     <div className="friends-list-wrapper">
+      <h1>Friends</h1>
       {props.friends.map(friend => {
         return (
           <div className="friend-card" key={friend.id}>
             <h2>{friend.name}</h2>
-            <p>{friend.age}</p>
-            <p>{friend.email}</p>
+            <div className="friend-info">
+              <p>Age: {friend.age}</p>
+              <p>Email: {friend.email}</p>
+            </div>
           </div>
         );
       })}
